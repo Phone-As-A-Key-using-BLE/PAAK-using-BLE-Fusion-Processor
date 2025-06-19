f20b746cb49c0f7b5ca0efc2b16235ba5abc8ea5
Anas Hamed Ahmed Hamed <111208532+Anas2108@users.noreply.github.com>
2025-06-19 17:11:34 +0300

    refactor: update Master_trilaterate_position parameter and add comments

--- APP/Fusion/Trilateration.h
+++ APP/Fusion/Trilateration.h
@@ -1,7 +1,14 @@
+/**
+ * @file Trilateration.h
+ * @brief Trilateration calculations for position estimation.
+ * 
+ * @author Anas Hamed, Mohamad Waleed and Ahmed Yousery
+ * @date [5/2/2025] d/m/y
+ */
+
+
 #ifndef TRILATERATION_H_
 #define TRILATERATION_H_
-
-// #include "helper_function.h"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,24 +16,57 @@
 #include <math.h>
 #include "measurement.h"
 #include "std_types.h"
-// #include "can_msg_types.h"
 
-#define NUM_OF_ANCHORS 3
+#define NUM_OF_ANCHORS 3 /**< Number of anchor nodes used for trilateration */
 
+/**
+ * @brief Structure representing an object detected by anchors.
+ */
+typedef struct  {
+    float32 distance; /**< Measured distance from the anchor to the object in cm */
+    uint8 mesgId;     /**< Message ID associated with the measurement */
+} object_list;
 
-
-typedef struct{
-	// uint8_t deviceId : 4;	/* Maximum 16 devices to be registered for one car */
-	float32 distance ;		/* Maximum 2048 cm ~ 20 m that the BLE can reach (to be checked again in the open space)*/	
-	uint8 mesgId;
-}object_list;
-
-//For RSSI and ToF
+/**
+ * @brief Multiplies two matrices (A * B = result).
+ *
+ * @param A Pointer to the first matrix.
+ * @param B Pointer to the second matrix.
+ * @param result Pointer to the resulting matrix.
+ * @param m Number of rows in matrix A.
+ * @param n Number of columns in matrix A and rows in matrix B.
+ * @param p Number of columns in matrix B.
+ */
 void matrix_multiply(float64 *A, float64 *B, float64 *result, sint32 m, sint32 n, sint32 p);
+
+/**
+ * @brief Computes the transpose of a matrix.
+ *
+ * @param A Pointer to the original matrix.
+ * @param result Pointer to the transposed matrix.
+ * @param n Number of rows in the original matrix.
+ * @param m Number of columns in the original matrix.
+ */
 void matrix_transpose(float64 *A, float64 *result, sint32 n, sint32 m);
+
+/**
+ * @brief Computes the inverse of a square matrix.
+ *
+ * @param A Pointer to the original matrix.
+ * @param result Pointer to the matrix where the inverse will be stored.
+ * @return 1 if the inversion is successful, 0 if the matrix is singular.
+ */
 sint32 matrix_inverse(float64 *A, float64 *result);
 
+/**
+ * @brief Estimates the position of an object using trilateration.
+ *
+ * This function takes the distances from multiple anchors and estimates the
+ * object's (x, y) position.
+ *
+ * @param arr_object_list Array of object_list structures containing distance measurements.
+ * @return The estimated position as a Measurement_Type structure.
+ */
 Measurement_Type Master_trilaterate_position(object_list* arr_object_list);
 
-
-#endif /* SENSOR_FUSION_H_ */
+#endif /* TRILATERATION_H_ */
--- APP/Fusion/trilateration.c
+++ APP/Fusion/trilateration.c
@@ -1,108 +1,133 @@
-#include "Trilateration.h"
 
+ #include "Trilateration.h"
 
-
-// Function to multiply two matrices A[m x n] and B[n x p] to result[m x p]
-void matrix_multiply(float64 *A, float64 *B, float64 *result, sint32 m, sint32 n, sint32 p) {
-    sint32 i,j,k;
-    for( i = 0; i < m; i++) {
-        for(j = 0; j < p; j++) {
-            float64 sum = 0;
-            for(k = 0; k < n; k++) {
-                sum += A[i*n + k] * B[k*p + j];
-            }
-            result[i*p + j] = sum;
-        }
-    }
-}
-
-// Function to transpose a matrix A[n x m] to result[m x n]
-void matrix_transpose(float64 *A, float64 *result, sint32 n, sint32 m) {
-    sint32 i,j;
-    for(i = 0; i < n; i++) {
-        for(j = 0; j < m; j++) {
-            result[j*n + i] = A[i*m + j];
-        }
-    }
-}
-
-// Function to invert a 2x2 matrix
-sint32 matrix_inverse(float64 *A, float64 *result) {
-    float64 det = A[0] * A[3] - A[1] * A[2];
-//    printf("A[0]=%f, A[3]=%f, A[1]=%f, A[2]=%f, det = %f\n", A[0], A[3], A[1], A[2], det);
-
-    if(det == 0) return -1; // Singular matrix, can't invert
-
-    float64 invDet = 1.0 / det;
-    result[0] = A[3] * invDet;
-    result[1] = -A[1] * invDet;
-    result[2] = -A[2] * invDet;
-    result[3] = A[0] * invDet;
-    return 0;
-}
-
-// Function to solve the least squares problem for trilateration
-Measurement_Type Master_trilaterate_position(object_list arr_object_list[NUM_OF_ANCHORS]) {
-    sint32 i;
-	Measurement_Type devicePosition;
-    for( i=0;i<NUM_OF_ANCHORS; i++){
-        object_list *p = &arr_object_list[i];
-
-        if(p->mesgId == 0 ){
-            devicePosition.x=-1;
-            devicePosition.y=-1;
-            devicePosition.type=-1;
-            // SHELL_PrintfSynchronization((shell_handle_t)g_shellHandle, "\r\ntrilate has anchor %d with mesgId == 0\r\n", i);
-            return devicePosition;
-        }
-    }
-    // SHELL_PrintfSynchronization((shell_handle_t)g_shellHandle, "\r\ntrilate has No anchor with mesgId == 0\r\n");
-    arr_object_list[0].mesgId = 0;
-	arr_object_list[1].mesgId = 0;
-	arr_object_list[2].mesgId = 0;
-
-    float64 A[NUM_OF_ANCHORS*2], B[NUM_OF_ANCHORS], AT[2*NUM_OF_ANCHORS], ATA[4], ATB[2], ATA_inv[4];
-
-    // Anchor coordinates 140cm x 80cm
-    Measurement anchorCoordinates[NUM_OF_ANCHORS] = {
-        {0.7,-0.4},    // Anchor1
-        {-0.7,0},   // Anchor2
-        {0.7,0.4}   // Anchor3
-    };
-//(x-x1)^2+(y-y1^2)=r^2
-    // Construct matrices A and B
-
-    for(i = 0; i < NUM_OF_ANCHORS; i++) {
-        A[i*2] = 2 * (anchorCoordinates[i].x - anchorCoordinates[NUM_OF_ANCHORS-1].x);
-        A[i*2 + 1] = 2 * (anchorCoordinates[i].y - anchorCoordinates[NUM_OF_ANCHORS-1].y);
-        B[i] = pow(arr_object_list[i].distance, 2) - pow(arr_object_list[NUM_OF_ANCHORS-1].distance, 2)
-               - pow(anchorCoordinates[i].x, 2) - pow(anchorCoordinates[i].y, 2)
-               + pow(anchorCoordinates[NUM_OF_ANCHORS-1].x, 2) + pow(anchorCoordinates[NUM_OF_ANCHORS-1].y, 2);
-    }
-
-    // Compute transposition of A to AT
-    matrix_transpose(A, AT, NUM_OF_ANCHORS, 2);
-
-    // Compute ATA and ATB for normal equations
-    matrix_multiply(AT, A, ATA, 2, NUM_OF_ANCHORS, 2);
-    matrix_multiply(AT, B, ATB, 2, NUM_OF_ANCHORS, 1);
-
-    if(matrix_inverse(ATA, ATA_inv) != 0) {
-
-            devicePosition.x=-1;
-            devicePosition.y=-1;
-            devicePosition.type=-1;
-            return devicePosition;
-    }
-
-    // Compute least squares solution
-    float64 solution[2];
-    matrix_multiply(ATA_inv, ATB, solution, 2, 2, 1);
-
-    devicePosition.x=solution[0]*-1;
-    devicePosition.y=solution[1]*-1;
-    devicePosition.type=0;
-
-    return devicePosition;
-}
-
+ /**
+  * @brief Multiplies two matrices A[m x n] and B[n x p] and stores the result in result[m x p].
+  * 
+  * @param A Pointer to the first matrix.
+  * @param B Pointer to the second matrix.
+  * @param result Pointer to store the multiplication result.
+  * @param m Number of rows in matrix A.
+  * @param n Number of columns in matrix A and rows in matrix B.
+  * @param p Number of columns in matrix B.
+  */
+ void matrix_multiply(float64 *A, float64 *B, float64 *result, sint32 m, sint32 n, sint32 p) {
+     sint32 i, j, k;
+     for (i = 0; i < m; i++) {
+         for (j = 0; j < p; j++) {
+             float64 sum = 0;
+             for (k = 0; k < n; k++) {
+                 sum += A[i * n + k] * B[k * p + j];
+             }
+             result[i * p + j] = sum;
+         }
+     }
+ }
+ 
+ /**
+  * @brief Transposes a matrix A[n x m] and stores the result in result[m x n].
+  * 
+  * @param A Pointer to the input matrix.
+  * @param result Pointer to store the transposed matrix.
+  * @param n Number of rows in A.
+  * @param m Number of columns in A.
+  */
+ void matrix_transpose(float64 *A, float64 *result, sint32 n, sint32 m) {
+     sint32 i, j;
+     for (i = 0; i < n; i++) {
+         for (j = 0; j < m; j++) {
+             result[j * n + i] = A[i * m + j];
+         }
+     }
+ }
+ 
+ /**
+  * @brief Inverts a 2x2 matrix.
+  * 
+  * @param A Pointer to the 2x2 matrix.
+  * @param result Pointer to store the inverted matrix.
+  * @return 0 if successful, -1 if the matrix is singular.
+  */
+ sint32 matrix_inverse(float64 *A, float64 *result) {
+     float64 det = A[0] * A[3] - A[1] * A[2];
+ 
+     if (det == 0) return -1; // Singular matrix, can't invert
+ 
+     float64 invDet = 1.0 / det;
+     result[0] = A[3] * invDet;
+     result[1] = -A[1] * invDet;
+     result[2] = -A[2] * invDet;
+     result[3] = A[0] * invDet;
+     return 0;
+ }
+ 
+ /**
+  * @brief Solves the least squares problem for trilateration.
+  * 
+  * @param arr_object_list Array of object_list structures containing anchor data.
+  * @return The calculated position as a Measurement_Type structure.
+  */
+ Measurement_Type Master_trilaterate_position(object_list* arr_object_list) {
+     sint32 i;
+     Measurement_Type devicePosition;
+     
+     for (i = 0; i < NUM_OF_ANCHORS; i++) {
+         object_list *p = &arr_object_list[i];
+ 
+         if (p->mesgId == 0) {
+             devicePosition.x = -1;
+             devicePosition.y = -1;
+             devicePosition.type = -1;
+             return devicePosition;
+         }
+     }
+ 
+     arr_object_list[0].mesgId = 0;
+     arr_object_list[1].mesgId = 0;
+     arr_object_list[2].mesgId = 0;
+ 
+     float64 A[NUM_OF_ANCHORS * 2], B[NUM_OF_ANCHORS], AT[2 * NUM_OF_ANCHORS], ATA[4], ATB[2], ATA_inv[4];
+ 
+     /**
+      * @brief Anchor coordinates used in the trilateration calculation.
+      */
+     Measurement anchorCoordinates[NUM_OF_ANCHORS] = {
+            {0.7, -0.4},   /**< First Node coordinates */
+            {-0.7, 0},     /**< Second anchor coordinates */
+            {0.7, 0.4}     /**< Third anchor coordinates */
+     };
+ 
+     // Construct matrices A and B
+     for (i = 0; i < NUM_OF_ANCHORS; i++) {
+         A[i * 2] = 2 * (anchorCoordinates[i].x - anchorCoordinates[NUM_OF_ANCHORS - 1].x);
+         A[i * 2 + 1] = 2 * (anchorCoordinates[i].y - anchorCoordinates[NUM_OF_ANCHORS - 1].y);
+         B[i] = pow(arr_object_list[i].distance, 2) - pow(arr_object_list[NUM_OF_ANCHORS - 1].distance, 2)
+                - pow(anchorCoordinates[i].x, 2) - pow(anchorCoordinates[i].y, 2)
+                + pow(anchorCoordinates[NUM_OF_ANCHORS - 1].x, 2) + pow(anchorCoordinates[NUM_OF_ANCHORS - 1].y, 2);
+     }
+ 
+     // Compute transposition of A to AT
+     matrix_transpose(A, AT, NUM_OF_ANCHORS, 2);
+ 
+     // Compute ATA and ATB for normal equations
+     matrix_multiply(AT, A, ATA, 2, NUM_OF_ANCHORS, 2);
+     matrix_multiply(AT, B, ATB, 2, NUM_OF_ANCHORS, 1);
+ 
+     if (matrix_inverse(ATA, ATA_inv) != 0) {
+         devicePosition.x = -1;
+         devicePosition.y = -1;
+         devicePosition.type = -1;
+         return devicePosition;
+     }
+ 
+     // Compute least squares solution
+     float64 solution[2];
+     matrix_multiply(ATA_inv, ATB, solution, 2, 2, 1);
+ 
+     devicePosition.x = solution[0] * -1;
+     devicePosition.y = solution[1] * -1;
+     devicePosition.type = 0;
+ 
+     return devicePosition;
+ }
+ 
